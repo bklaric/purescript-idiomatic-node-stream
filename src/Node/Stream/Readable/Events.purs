0b45a60c587cc18e4e41f4f48a81a934336a2fb4
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-04-25 20:56:59 +0200

    Remove unsafe events

--- src/Node/Stream/Readable/Events.purs
+++ src/Node/Stream/Readable/Events.purs
@@ -2,10 +2,11 @@ module Node.Stream.Readable.Events where
 
 import Prelude
 
-import Effect (Effect)
 import Data.Foreign (Foreign)
+import Effect (Effect)
 import Node.Buffer (Buffer)
 import Node.Events.Event (Event(..))
+import Node.Events.EventEmitter (on')
 import Node.Stream.Readable (class Readable)
 
 close :: forall readable. Readable readable =>
@@ -15,14 +16,6 @@ close = Event "close"
 data' :: forall readable. Readable readable =>
     Event readable (Foreign -> Effect Unit)
 data' = Event "data"
-
-dataBuffer :: forall readable. Readable readable =>
-    Event readable (Buffer -> Effect Unit)
-dataBuffer = Event "data"
-
-dataString :: forall readable. Readable readable =>
-    Event readable (String -> Effect Unit)
-dataString = Event "data"
 
 end :: forall readable. Readable readable =>
     Event readable (Effect Unit)
